ce8935133cd05486f12ab298f05e26805b872400
odedns <oded.nissan@gmail.com>
2024-08-27 14:01:55 +0300

    test

---
+++ api-analyzer/tests/sample_bedrock.py
@@ -0,0 +1,37 @@
+# Use the Conversation API to send a text message to Amazon Titan Text.
+
+import boto3
+from botocore.exceptions import ClientError
+
+# Create a Bedrock Runtime client in the AWS Region you want to use.
+client = boto3.client("bedrock-runtime", region_name="us-west-2")
+
+# Set the model ID, e.g., Titan Text Premier.
+model_id = "amazon.titan-text-lite-v1"
+
+# Start a conversation with the user message.
+user_message = "Describe the purpose of a 'hello world' program in one line."
+conversation = [
+    {
+        "role": "user",
+        "content": [{"text": user_message}],
+    }
+]
+
+try:
+    # Send the message to the model, using a basic inference configuration.
+    response = client.converse(
+        modelId=model_id,
+        messages=conversation,
+        inferenceConfig={"maxTokens": 512, "temperature": 0.5, "topP": 0.9},
+    )
+
+    # Extract and print the response text.
+    response_text = response["output"]["message"]["content"][0]["text"]
+    print(response_text)
+
+except (ClientError, Exception) as e:
+    print(f"ERROR: Can't invoke '{model_id}'. Reason: {e}")
+    exit(1)
+
+
