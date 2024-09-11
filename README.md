1713c4f2807935a4309bb7f3761ec336f22cf1a9
odedns <oded.nissan@gmail.com>
2024-09-11 10:07:58 +0300

    last fix

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 This is a repository for the AI competition.
 It contains a python project template.
+streamlit run /home/oded/git/ai-demo/api-analyzer/src/webapp.py 
--- api-analyzer/src/webapp.py
+++ api-analyzer/src/webapp.py
@@ -7,10 +7,14 @@ st.title("Swagger Analyzer")
 st.header("Generates a report analyzing the input swagger")
 
 
+#st.markdown(html_string, unsafe_allow_html=True)
+
 
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', encoding='utf-8', level=logging.INFO)
-model_id="amazon.titan-text-express-v1"
+#model_id="amazon.titan-text-express-v1"
+model_id="anthropic.claude-instant-v1"
+
 client = bedrock_client(model_id)
 
 
@@ -18,7 +22,7 @@ def generate_response(input_text):
   response = client.analyze(input_text)    
   logging.info("response: %s",response)
   st.info(response)
-
+  #st.markdown(response, unsafe_allow_html=True)
 with st.form('my_form'):
   text = st.text_area('Enter swagger file to analyze:')
   submitted = st.form_submit_button('Submit')
